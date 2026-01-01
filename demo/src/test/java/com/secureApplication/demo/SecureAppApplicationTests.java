95761a4175996758d80f713cdb30050dc4df8d07
Divyansh Sharma <divyansh.sharma09893@gmail.com>
2026-01-01 19:40:33 +0000

    project initialization

---
+++ demo/src/test/java/com/secureApplication/demo/SecureAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secureApplication.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
