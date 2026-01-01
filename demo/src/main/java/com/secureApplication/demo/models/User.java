1b3804429f610eb8e851a8dd15517931609db19c
Divyansh Sharma <divyansh.sharma09893@gmail.com>
2026-01-01 19:56:53 +0000

    Insecure User
    added insecure user entity with plain text password

---
+++ demo/src/main/java/com/secureApplication/demo/models/User.java
@@ -0,0 +1,18 @@
+package com.secureApplication.demo.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username;
+    private String password; // stored as plain text insecure
+    private String email;
+
+}
